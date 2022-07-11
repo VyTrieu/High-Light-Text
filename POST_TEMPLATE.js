087642d510bfa5ab9d6f227f35fd73991ce6d950
Rosaline <gialang48@gmail.com>
2022-07-11 23:33:56 +0700

    Add files via upload

---
+++ POST_TEMPLATE.js
@@ -0,0 +1,132 @@
+function createPagination(totalPages, page) {
+  let liTag = "";
+  let active;
+  let beforePage = page - 1;
+  let afterPage = page + 1;
+
+  if (page > 1) {
+    //hiện nút lùi về nếu trang hiện tại lớn hơn 1
+    liTag += `<li id= "#vol_${page - 1}" onclick="show(this.id);createPagination(totalPages, ${page - 1})">|◁</li>`;
+  }
+
+  if (page > 2 && totalPages >= 5) {
+    //trang hiện tại lớn hơn 2 thì hiện vol. 1 đầu tiên
+    liTag += `<li id= "#vol_1" onclick="show(this.id);createPagination(totalPages, 1)">Vol. 1</li>`;
+    if (page > 3 && totalPages > 5) {
+      //trang hiện tại lớn hơn 3 và tổng trang > 5 thì hiện ... phía trước
+      liTag += `<li>...</li>`;
+    }
+  }
+
+  // hiện bao nhiêu trang trước trang hiện tại, lưu ý chỉ áp dụng cho tổng trang & trang từ 5 trở lên
+  if (page == totalPages) {
+    beforePage = beforePage - 2;
+  } else if (page == totalPages - 1) {
+    beforePage = beforePage - 1;
+  }
+  // hiện bao nhiêu trang sau trang hiện tại, lưu ý chỉ áp dụng cho tổng trang & trang từ 5 trở lên
+  if (page == 1) {
+    afterPage = afterPage + 2;
+  } else if (page == 2) {
+    afterPage = afterPage + 1;
+  }
+
+  //Tự động tạo mục lục
+  for (var i = beforePage; i <= afterPage; i++) {
+    if (i > totalPages) {
+      //i lớn hơn trang tổng --> ngắt, ko tự tạo thêm liTag
+      continue;
+    }
+    if (i == 0) {
+      //nếu i = 0 && beforePage = 0 --> sẽ tự + 1
+      i = i + 1;
+    }
+    if (i == page) {
+      //i = trang hiện tại --> thêm class active
+      active = "active";
+    } else {
+      //còn lại xóa class active
+      active = "";
+    }
+    liTag += `<li id= "#vol_${i}" class="${active}" onclick="show(this.id); createPagination(totalPages, ${i})">Vol. ${i}</li>`;
+  }
+
+  if (page < totalPages - 1 && totalPages >= 5) {
+    //trang hiện tại nhỏ hơn tổng trang - 1
+    if (page < totalPages - 2 && totalPages > 5) {
+      //thêm ... khi trang hiện tại < tổng trang -2 và tổng trang > 5
+      liTag += `<li>...</li>`;
+    }
+    liTag += `<li id= "#vol_${totalPages}" onclick="show(this.id); createPagination(totalPages, ${totalPages})">Vol. ${totalPages}</li>`;
+  }
+
+  if (page < totalPages) {
+    //trang hiện tại < tổng trang --> hiện next button
+    liTag += `<li id= "#vol_${page + 1}" onclick="show(this.id); createPagination(totalPages, ${page + 1})">▷|</li>`;
+  }
+  element.innerHTML = liTag; //cập nhật class active
+  return liTag;
+}
+
+function show(vol) {
+  vol = document.getElementById(vol.substr(vol.length - 5));
+  for (let i = 1; i <= totalPages; i++) {
+    let temp = str + i;
+    if (temp != vol) {
+      document.getElementById(temp).style.display = "none";
+    }
+  }
+  document.getElementById(vol.id).style.display = "block";
+}
+
+function highLight(chapter, regex) {
+  let array = chapter.getElementsByTagName("a");
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i].innerText.search(regex) == 0){
+      array[i].classList.add("highlight");
+    }
+    else {
+      array[i].classList.remove("highlight");
+    }
+    console.log(array[i]);
+  }
+}
+
+function goToChapter() {
+  let found = false;
+  var content = document.querySelectorAll(".listChapter");
+  let chapter = "Chương " + document.getElementById("chapter").value;
+  let strRaw = String.raw `(${chapter}\s).*`;
+  let regex = new RegExp(strRaw,'gi');
+
+  if (chapter == "Chương ") {
+    chapter = 0;
+  }
+
+  if (chapter == 0) {
+    alert("Vui lòng nhập số chương cần đọc");
+    found = true;
+  } else {
+    for (let obj of content) {
+      if (obj.innerText.search(regex) > 0) {
+        page = parseInt(obj.id.substring(obj.id.length - 1));
+        found = true;
+        highLight(obj, regex);
+      }
+      else {
+        highLight(obj, regex);
+      }
+    }
+  }
+  if (found == false) {
+    alert("Chương bạn kiếm hiện không có");
+  }
+  element.innerHTML = createPagination(totalPages, page);
+  show("vol_" + page);
+}
+
+function getEnter(e) {
+  if (event.keyCode == 13) {
+    goToChapter();
+  }
+}
